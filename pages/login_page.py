7918d33ff7046b412c8e6a6ed402bc540939010a
andrey <andkr5150@gmail.com>
2019-12-07 01:34:58 -0800

    add log and reg test

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # проверка на корректный url адрес
+        assert "login" in self.browser.current_url, "url - login is NOT correct"
+
+    def should_be_login_form(self):
+        # проверка, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login FORM is NOT correct"
+
+    def should_be_register_form(self):
+        # проверка, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register FORM is NOT correct"
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 
@@ -15,3 +17,11 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
+
+def test_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+    page.should_be_login_form()
+    page.should_be_register_form()
